fc397d2dcb38d23ff2c931280647cf0dd321efa5
alphadeveloper <alphadeveloper622@gmail.com>
2023-01-27 19:22:53 +0900

    add pie chart

--- src/components/DrilldownChart.vue
+++ src/components/DrilldownChart.vue
@@ -1,6 +1,12 @@
 <template>
-  <Doughnut ref="chartRef" :data="chartData" :options="chartConfig.options" @click="onClick"/>
-  <!-- <Pie :data="chartData" :options="chartConfig.options" /> -->
+  <div style="display:flex">
+  <div class="chart-container" style="position: relative; height:60vh; width:100%">
+  <Doughnut id="doughnut" ref="chartRef" :data="chartData" :options="chartConfig.options" @click="onClick"/>
+  </div>
+  <div class="chart-container" style="position: relative; height:60vh; width:100%">
+  <Pie id="pie" :data="chartData" :options="chartConfig.options" @click="onClickPie"/>
+  </div>
+  </div>
   <!-- <div class="col-span-6 sm:col-span-3">
     <label for="country" class="block text-sm font-medium text-gray-700">Country</label>
     <select id="country" name="country" autocomplete="country-name" class="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
@@ -134,7 +140,13 @@ export default {
    
       this.elementAtEvent(getElementAtEvent(chart, event));
     
-      //this.chartData=this.chartData2;
+    },
+    onClickPie(event: MouseEvent) {
+      console.log(this.$refs.chartRef);
+      const chart = this.$refs.chartRef.chart;
+   
+      this.elementAtEvent(getElementAtEvent(chart, event));
+    
     },
     elementAtEvent(element: InteractionItem[]){
       if (!element.length) return;
